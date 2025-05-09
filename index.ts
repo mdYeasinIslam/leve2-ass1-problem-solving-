4d098d834d88ca8248c6c980cf49f8767fb05d4d
mdYeasinIslam <hyeasinislam@gmail.com>
2025-05-09 22:38:40 +0600

    first problem solve

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log('Hello, world!');
+{
+    function formatString(text: string, toUpperCase?: boolean): string {
+        if(toUpperCase === false){
+            return text.toLowerCase();
+        }else {
+            return text.toUpperCase();
+        }
+    }
+
+
+    
+}
