92c630a7551c08a7242b435ca6e3db7f276c9ae2
mdYeasinIslam <hyeasinislam@gmail.com>
2025-05-09 22:49:26 +0600

    7th problem solved

--- index.ts
+++ index.ts
@@ -70,6 +70,22 @@
         return expensive;
     }
 
-   
+    enum Day {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+
+    function getDayType(day: Day): string {
+        if(day === Day.Saturday || day === Day.Sunday) {
+            return "Weekend";
+        }
+        return "Weekday";
+    }
+
 
 }
