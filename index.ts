70e7dd6b8446da9a3fede1eb9e23c6e6c06b4a99
mdYeasinIslam <hyeasinislam@gmail.com>
2025-05-09 22:51:02 +0600

    final problem solved

--- index.ts
+++ index.ts
@@ -88,4 +88,15 @@
     }
 
 
+    async function squareAsync(n: number): Promise<number> {
+        return new Promise((resolve, reject) => {
+            if( n < 0){
+                reject(new Error("Negative number not allowed"));
+            }
+            setTimeout(() => {
+                resolve(n * n);
+            }, 1000);
+        })
+        
+    }
 }
